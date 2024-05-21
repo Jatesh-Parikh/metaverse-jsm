a5e2b86ea3bfbdaff2ea14e2b871c0207801236e
Jatesh Parikh <jateshparikh97@gmail.com>
2024-05-21 14:56:49 +0530

    about section completed

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,22 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <About />
-      <Explore />
-      <GetStarted />
-      <WhatsNew />
+      <div className="relative">
+        <About />
+        <div className="gradient-03 z-0" />
+        <Explore />
+      </div>
+      <div className="relative">
+        <GetStarted />
+        <div className="gradient-04 z-0" />
+        <WhatsNew />
+      </div>
       <World />
-      <Insights />
-      <Feedback />
+      <div className="relative">
+        <Insights />
+        <div className="gradient-04 z-0" />
+        <Feedback />
+      </div>
     </>
   );
 }
