e0213c88f312d9164054d648af94d71a7efa0958
Jatesh Parikh <jateshparikh97@gmail.com>
2024-05-21 13:22:23 +0530

    project setup

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../index";
+import { navVariants } from "../utils/motion";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
